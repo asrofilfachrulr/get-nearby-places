3dff8dccdb84302dc516f82a34d79e57e067096e
asrofil <riidloa@gmail.com>
2022-04-07 16:16:42 +0700

    following instruction

--- models/category.go
+++ models/category.go
@@ -1,27 +1,26 @@
 package models
 
-type (
-	Category struct {
-		ID    uint8
-		Name  string
-		Count uint
-	}
-)
+type Category struct {
+	ID    uint8
+	Name  string
+	Count uint
+}
 
+// Predefined categories, mapped by their region level
 var MapCategories = map[string][]Category{
 	"city": {
-		{ID: 1, Name: "Rumah Sakit", Count: 3},
-		{ID: 2, Name: "SMA", Count: 20},
-		{ID: 3, Name: "Kantor Pemerintah", Count: 1},
+		{ID: 1, Name: "Kantor Pemerintah", Count: 1},
+		{ID: 2, Name: "Rumah Sakit", Count: 3},
+		{ID: 3, Name: "SMA", Count: 20},
 	},
 	"district": {
-		{ID: 1, Name: "Puskesmas", Count: 5},
-		{ID: 2, Name: "SMP", Count: 3},
-		{ID: 3, Name: "Kantor Pemerintah Kecamatan", Count: 1},
+		{ID: 1, Name: "Kantor Pemerintah Kecamatan", Count: 1},
+		{ID: 2, Name: "Puskesmas", Count: 5},
+		{ID: 3, Name: "SMP", Count: 3},
 	},
 	"village": {
-		{ID: 1, Name: "SD", Count: 5},
+		{ID: 1, Name: "Kantor Pemerintah", Count: 1},
 		{ID: 2, Name: "Tempat Ibadah", Count: 20},
-		{ID: 3, Name: "Kantor Pemerintah", Count: 1},
+		{ID: 3, Name: "SD", Count: 5},
 	},
 }
